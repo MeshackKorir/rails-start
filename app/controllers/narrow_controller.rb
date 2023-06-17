87da0ac29789b6236273e9b2ece4945a87ca5869
Meshack Korir <meshackkorir100@gmail.com>
2023-06-17 16:02:25 +0300

    done grt/json

--- app/controllers/narrow_controller.rb
+++ app/controllers/narrow_controller.rb
@@ -1,7 +1,10 @@
 class NarrowController < ApplicationController
 
     def index
-        render json: { hello: "cheese world" }
+        render json: { hello: "narrow world" }
     end
   
   end
+
+  # i have learnt on how to use rails to send back HTML or JSON as a response
+  # 
